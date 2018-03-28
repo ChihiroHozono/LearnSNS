693646590c3bee4c7235bb8f00a4875259fac5df
ChihiroHozono <chihiro4434132@gmail.com>
2018-03-28 11:46:43 +0800

    feedをデータベースに保存

--- timeline.php
+++ timeline.php
@@ -1,1 +1,152 @@
-Timeline.phpだす
+<?php
+  session_start();
+
+  require('db_connect.php');
+  // ナビバーに表示するためログインユーザーの情報を取得
+  $sql = 'SELECT * FROM `users` WHERE `id` ='.$_SESSION['id'];
+
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+
+  $login_user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+  // セッション変数にユーザーのidが入っているのか
+  // var_dump($login_user);
+  // imgファイルのパス
+  $img_path = 'user_profile_img/'.$login_user['img_name'];
+
+  // つぶやきを保存
+  if(isset($_POST) && !empty($_POST)){
+    // var_dump($_POST["feed"]);
+    // データベースに接続
+    // 変数に代入
+    $feed = $_POST['feed'];
+    $user_id = $login_user['id'];
+    $img_name = $img_path;
+
+    require('db_connect.php');
+
+    $sql = 'INSERT INTO `feeds` SET `feed`=?, `user_id`=?,`created`= NOW()';
+      // インサート実行
+    $data = array($feed,$user_id);
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+  }
+
+  // つぶやきをデータベースから取得
+  // 必要なデータ
+  // 画像、名前、コメントuser_idとコメントのidを結びつける
+    $sql = 'SELECT * FROM `feeds`';
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Learn SNS</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body style="margin-top: 60px; background: #E4E6EB;">
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/">Learn SNS</a>
+      </div>
+      <div class="collapse navbar-collapse" id="navbar-collapse1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">タイムライン</a></li>
+          <li><a href="#">ユーザー一覧</a></li>
+        </ul>
+        <form method="GET" action="" class="navbar-form navbar-left" role="search">
+          <div class="form-group">
+            <input type="text" name="search_word" class="form-control" placeholder="投稿を検索">
+          </div>
+          <button type="submit" class="btn btn-default">検索</button>
+        </form>
+        <ul class="nav navbar-nav navbar-right">
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="<?php echo $img_path; ?>" width="18" class="img-circle"><?php echo $login_user['name']; ?><span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">マイページ</a></li>
+              <li><a href="signout.php">サインアウト</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-3">
+        <ul class="nav nav-pills nav-stacked">
+          <li class="active"><a href="timeline.php?feed_select=news">新着順</a></li>
+          <li><a href="timeline.php?feed_select=likes">いいね！済み</a></li>
+          <!-- <li><a href="timeline.php?feed_select=follows">フォロー</a></li> -->
+        </ul>
+      </div>
+      <div class="col-xs-9">
+        <div class="feed_form thumbnail">
+          <form method="POST" action="timeline.php">
+            <div class="form-group">
+              <textarea name="feed" class="form-control" rows="3" placeholder="Happy Hacking!" style="font-size: 24px;"></textarea><br>
+            </div>
+            <input type="submit" value="投稿する" class="btn btn-primary">
+          </form>
+        </div>
+          <div class="thumbnail">
+            <div class="row">
+              <div class="col-xs-1">
+                <img src="https://placehold.jp/40x40.png" width="40">
+              </div>
+              <div class="col-xs-11">
+                名前 aaa<br>
+                <a href="#" style="color: #7F7F7F;">2018-03-03</a>
+              </div>
+            </div>
+            <div class="row feed_content">
+              <div class="col-xs-12" >
+                <span style="font-size: 24px;">つぶやき</span>
+              </div>
+            </div>
+            <div class="row feed_sub">
+              <div class="col-xs-12">
+                <form method="POST" action="" style="display: inline;">
+                  <input type="hidden" name="feed_id" >
+
+                    <input type="hidden" name="like" value="like">
+                    <button type="submit" class="btn btn-default btn-xs"><i class="fa fa-thumbs-up" aria-hidden="true"></i>いいね！</button>
+                </form>
+                <span class="like_count">いいね数 : 100</span>
+                <span class="comment_count">コメント数 : 9</span>
+                  <a href="#" class="btn btn-success btn-xs">編集</a>
+                  <a href="#" class="btn btn-danger btn-xs">削除</a>
+              </div>
+         a   </div>
+          </div>
+        <nav aria-label="Page navigation">
+          <ul class="pager">
+            <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Older</a></li>
+            <li class="next"><a href="#">Newer <span aria-hidden="true">&rarr;</span></a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+  <script src="assets/js/jquery-3.1.1.js"></script>
+  <script src="assets/js/jquery-migrate-1.4.1.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
